0574d4e12dabdab06dc58a595df027babdd56dba
pradeep <36475808+kloudvsol@users.noreply.github.com>
2025-06-06 08:54:39 +0530

    Update webserer_userdata.sh

--- webserer_userdata.sh
+++ webserer_userdata.sh
@@ -2,7 +2,7 @@
 yum install httpd -y 
 systemctl start httpd ;
 systemctl enable httpd ;
-wget https://templatemo.com/tm-zip-files-2020/templatemo_584_pod_talk.ziphi   ;
+wget https://templatemo.com/tm-zip-files-2020/templatemo_584_pod_talk.zip  ;
 unzip templatemo_584_pod_talk.zip
 cp -r templatemo_584_pod_talk/* /var/www/html/
 systemctl restart httpd ;
